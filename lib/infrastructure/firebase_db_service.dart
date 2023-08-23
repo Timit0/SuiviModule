9b614c26330d8547498dae31713d661590b16602
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2023-08-23 08:01:58 +0200

    ajout de ce qu'il faut pour lancer la firebase

---
+++ lib/infrastructure/firebase_db_service.dart
@@ -0,0 +1,4 @@
+class FirebaseDBService {
+  final url = 'http://10.0.2.2:9000/eleve';
+  final dbName = '/?ns=suivimodule-default-rtdb';
+}
---
+++ runFirebase.bat
@@ -0,0 +1,4 @@
+REM juste pour faire tourner la firebase
+
+cd backend
+firebase emulators:start --import=./saved-data --export-on-exit
