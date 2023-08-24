7556bbf373b0d93cbf4a51388156c37e521328dd
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2023-08-24 14:41:01 +0200

    ajout d'un html.window.location.reload(); afin de pouvoir rafraichir la totalitee de la page sans s'enquiquiner avec le setState :)

--- lib/screen/student_list_screen.dart
+++ lib/screen/student_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html' as html; // window.reload.location.reload();
+
 import 'package:flutter/material.dart';
 import 'package:suivi_de_module/infrastructure/firebase_db_service.dart';
 
@@ -17,10 +19,12 @@ class StudentListScreen extends StatefulWidget {
 
   final _formKey = GlobalKey();
 
-  late String tempID;
-  late String tempName;
-  late String tempNickname;
-  late String tempPhotoUrl;
+  String tempID = "";
+  String tempName = "";
+  String tempNickname = "";
+  String tempPhotoUrl = "";
+
+  bool valid = true;
 
   @override
   State<StudentListScreen> createState() => _StudentListScreenState();
@@ -56,18 +60,45 @@ class _StudentListScreenState extends State<StudentListScreen> {
         ),
       ),
     ), floatingActionButton: FloatingActionButton(onPressed: () {
-        showDialog(context: context, builder: (context) => AlertDialog(
-        scrollable: true,
-        title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [const Text('Ajout d\'un élève'), IconButton(onPressed: () => Navigator.of(context).pop(), icon: const Icon(Icons.close))]),
-        content: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('identifiant', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempID = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('nom', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempName = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('prénom', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempNickname = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('URL de l\'image', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempPhotoUrl = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
+        showDialog(context: context, builder: (context) => StatefulBuilder( // afin qu'on puisse faire un setState mais QUE pour ce widget - là
+          builder:(context, setState) => AlertDialog(
+          scrollable: true,
+          title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [const Text('Ajout d\'un élève'), IconButton(onPressed: () => Navigator.of(context).pop(), icon: const Icon(Icons.close))]),
+          content: Column(children: [
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('identifiant', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempID = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('nom', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempName = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('prénom', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempNickname = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [const Text('URL de l\'image', textAlign: TextAlign.left), Padding(padding: const EdgeInsets.only(left: 8, bottom: 8), child: SizedBox(width: 300, child: TextField(onChanged: (value) {widget.tempPhotoUrl = value;}, decoration: const InputDecoration(border: OutlineInputBorder()))))]),
+        
+            TextButton(onPressed: () {
+              if
+              (
+                widget.tempID != "" &&
+                widget.tempName != "" &&
+                widget.tempNickname != ""
+              )
+              {
+                Eleve newEleve = Eleve(id: widget.tempID, name: widget.tempName, firstname: widget.tempNickname, photoFilename: widget.tempPhotoUrl == "" ? "assets/img/placeholderImage.png" : widget.tempPhotoUrl);
+        
+                widget.db.addEleve(newEleve);
+                widget.valid = true;
+                
+                Navigator.of(context).pop();
 
-          TextButton(onPressed: (){}, style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey), foregroundColor: MaterialStateProperty.all(Colors.white)), child: const Text('OK'))
-        ])
-      ));
+                // vue qu'on fait sur le web
+                html.window.location.reload();
+              }
+              else
+              {        
+                setState(() {
+                  widget.valid = false;
+                });
+              }
+            }, style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.grey), foregroundColor: MaterialStateProperty.all(Colors.white)), child: const Text('OK')),
+            Text((widget.valid == false ? 'L\'identifiant, Le nom ainsi que le prénom doient être remplit!' : ''), style: const TextStyle(color: Colors.red))
+          ])
+              ),
+        ));
     }, backgroundColor: const Color.fromARGB(255, 73, 73, 73), child: const Icon(Icons.person_add)));
   }
 }
