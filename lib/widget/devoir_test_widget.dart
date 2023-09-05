c23ac8bd0026886e30ac64811e81035014978fb3
Ogan Oezkul <Ogan.Oezkul@ceff.ch>
2023-09-05 13:19:34 +0200

    construction de la screen devoirTestWidget: fait!

--- lib/widget/devoir_test_widget.dart
+++ lib/widget/devoir_test_widget.dart
@@ -18,21 +18,30 @@ class _DevoirTestWidgetState extends State<DevoirTestWidget> {
     final ScrollController controllerDevoir = ScrollController();
     final ScrollController controllerTest = ScrollController();
 
-    return Column(children: [
-      const Text('Devoirs', textAlign: TextAlign.left),
-      ListOfWidget(
-        widget: CardWidget(type: CardState.Devoir), 
-        controller: controllerDevoir, 
-        varDebug: 8, 
-        paddingList: 50
-      ),
-      const Text('Tests', textAlign: TextAlign.left),
-      ListOfWidget(
-        widget: CardWidget(type: CardState.Test), 
-        controller: controllerTest, 
-        varDebug: 8, 
-        paddingList: 50
-      )
-    ]);
+    return Padding(
+      padding: const EdgeInsets.only(left: 80, right: 80, bottom: 100),
+      child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, crossAxisAlignment: CrossAxisAlignment.start, children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 45),
+          child: Text('Devoirs', textAlign: TextAlign.left, style: TextStyle(fontSize: 40)),
+        ),
+        ListOfWidget(
+          widget: CardWidget(type: CardState.Devoir), 
+          controller: controllerDevoir, 
+          varDebug: 8, 
+          paddingList: 50
+        ),
+        const Padding(
+          padding: EdgeInsets.only(left: 45),
+          child: Text('Tests', textAlign: TextAlign.left, style: TextStyle(fontSize: 40)),
+        ),
+        ListOfWidget(
+          widget: CardWidget(type: CardState.Test), 
+          controller: controllerTest, 
+          varDebug: 8, 
+          paddingList: 50
+        )
+      ]),
+    );
   }
 }
